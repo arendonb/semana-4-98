5c8b77236ffda4ffa05583d9a9e9bb8f761e99b9
arendonb <arendonb@gmail.com>
2020-12-18 18:32:36 -0500

    Semana 5 - Adiciona funciones al usuario

--- routes/api/usuario.js
+++ routes/api/usuario.js
@@ -2,9 +2,15 @@ const router = require('express').Router();
 const userController = require('../../controllers/UserController.js');
 const auth = require('../../middlewares/auth')
 
-router.get('/list', auth.verificarAdministrador, userController.list);
+/* router.get('/list', auth.verificarAdministrador, userController.list);
 router.post('/register', auth.verificarAdministrador, userController.register);
-router.put('/update', auth.verificarAdministrador, userController.update);
+router.put('/update', auth.verificarAdministrador, userController.update); */
+
+router.get('/list', userController.list);
+router.post('/add', userController.add);
+router.put('/update', userController.update);
+router.put('/activate', userController.activate);
+router.put('/deactivate', userController.deactivate);
 
 
 router.post('/login', userController.login);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const apiRouterUser = require('./api/usuario.js');
 const apiRouterCategoria = require('./api/categoria.js');
 const apiRouterArticulo = require('./api/articulo.js');
 
-router.use('/usuario', apiRouterUser); //.com/api/user
+router.use('/usuario', apiRouterUser);
 router.use('/categoria', apiRouterCategoria);
 router.use('/articulo', apiRouterArticulo);
 
