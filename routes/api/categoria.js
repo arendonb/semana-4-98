cfbba7e664f6ebf9bce7d245e4b13b46ce5aa85b
arendonb <arendonb@gmail.com>
2020-12-18 10:50:45 -0500

    se intento al go pero no funciono

--- routes/api/categoria.js
+++ routes/api/categoria.js
@@ -2,16 +2,16 @@ const router = require('express').Router();
 const categoriaController = require('../../controllers/CategoriaController');
 const auth = require('../../middlewares/auth')
 
-router.get('/list', auth.verificarVendedor, categoriaController.list);
+/* router.get('/list', auth.verificarVendedor, categoriaController.list);
 router.post('/add', auth.verificarVendedor, categoriaController.add);
 router.put('/update', auth.verificarVendedor, categoriaController.update);
 router.put('/activate', auth.verificarVendedor, categoriaController.activate);
-router.put('/deactivate', auth.verificarVendedor, categoriaController.deactivate);
+router.put('/deactivate', auth.verificarVendedor, categoriaController.deactivate); */
 
-/* router.get('/list', categoriaController.list);
+router.get('/list', categoriaController.list);
 router.post('/add', categoriaController.add);
 router.put('/update', categoriaController.update);
 router.put('/activate', categoriaController.activate);
-router.put('/deactivate', categoriaController.deactivate); */
+router.put('/deactivate', categoriaController.deactivate);
 
 module.exports=router;
