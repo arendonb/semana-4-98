da0e280bfe5bef46597785bdef6415220a2aa14e
arendonb <arendonb@gmail.com>
2020-12-21 14:54:33 -0500

    ensayo auth fallido

--- routes/api/usuario.js
+++ routes/api/usuario.js
@@ -3,8 +3,10 @@ const userController = require('../../controllers/UserController.js');
 const auth = require('../../middlewares/auth')
 
 /* router.get('/list', auth.verificarAdministrador, userController.list);
-router.post('/register', auth.verificarAdministrador, userController.register);
-router.put('/update', auth.verificarAdministrador, userController.update); */
+router.post('/add', auth.verificarAdministrador, userController.add);
+router.put('/update', auth.verificarAdministrador, userController.update);
+router.put('/activate', auth.verificarAdministrador, userController.activate);
+router.put('/deactivate', auth.verificarAdministrador, userController.deactivate); */
 
 router.get('/list', userController.list);
 router.post('/add', userController.add);
